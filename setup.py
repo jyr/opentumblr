61075e21ed2a1a08e0e72a24b24b34fe969242f7
Jair Gaxiol <jyr.gaxiola@gmail.com>
2009-04-04 16:51:05 -0600

    Preparando 0.0.1

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ path_images = os.getcwd() + '/opentumblr/images/'
 
 image_files = [path_images+'audio.png',path_images+'chat.png',path_images+'link.png',path_images+'photo.png',path_images+'quote.png',path_images+'text.png',path_images+'video.png']
 doc_files =['AUTHORS','INSTALL','LICENSE','README','THANKS']
+icon_files = ['opentumblr/images/opentumblr.png','opentumblr/images/opentumblr.xpm']
 
 if not os.path.isdir(ipath_docs):
     os.mkdir(ipath_docs)
@@ -21,7 +22,7 @@ if not os.path.isdir(ipath_images):
     os.mkdir(ipath_dashboard)
 
 setup(name="opentumblr",
-      version="BETA",
+      version="0.0.1",
       description="Cliente de escritorio para tumblr",
       author="Jair Gaxiola",
       author_email="jyr.gaxiola@gmail.com",
@@ -30,5 +31,5 @@ setup(name="opentumblr",
       scripts=['opentumblr/login.py'],
       packages=['opentumblr'],
       py_modules=['tumblr'],
-      data_files=[(ipath_dashboard, image_files),(ipath_docs, doc_files),(ipath_desktop,['opentumblr.desktop'])]
+      data_files=[('/usr/share/pixmaps/', icon_files),(ipath_dashboard, image_files),(ipath_docs, doc_files),(ipath_desktop,['opentumblr.desktop'])]
 )

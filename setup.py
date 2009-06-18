76ff592d5168eb612eb8ba1d1717acf4c5300b55
Jair Gaxiola <jyr@mac.gateway.2wire.net>
2009-06-17 20:52:19 -0500

    Cambiando el path de imagenes

--- setup.py
+++ setup.py
@@ -68,10 +68,10 @@ else:
 		ipath_dashboard = ipath_images+'dashboard/'
 		ipath_desktop = '/usr/share/applications/'
 
-		path_images = os.getcwd() + '/opentumblr/images/'
+		path_images = os.getcwd() + '/images/'
 		image_files = [path_images+'audio.png',path_images+'chat.png',path_images+'link.png',path_images+'photo.png',path_images+'quote.png',path_images+'text.png',path_images+'video.png']
 		doc_files =['AUTHORS','INSTALL','LICENSE','README','THANKS']
-		icon_files = ['opentumblr/images/opentumblr.png','opentumblr/images/opentumblr.xpm']
+		icon_files = ['images/opentumblr.png','images/opentumblr.xpm']
 
 		if not os.path.isdir(ipath_docs):
 			os.mkdir(ipath_docs)

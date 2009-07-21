ca22438fc2b6d4e7bcb4b0227fec9492ca7f441f
Jair Gaxiola <jyr@debian.(none)>
2009-07-21 14:58:50 -0500

    Uninstall opentumblr at linux

---
+++ uninstall.sh
@@ -0,0 +1,9 @@
+rm -rf /usr/share/doc/opentumblr
+rm -rf /usr/share/pixmaps/opentumblr
+rm -rf /usr/share/applications/opentumblr.desktop
+rm /usr/bin/opentumblr-client.py
+rm -rf /usr/lib/python2.5/site-packages/opentumblr
+rm /usr/share/pixmaps/opentumblr.*
+rm /usr/lib/python2.5/site-packages/opentumblr_client-0.0.4.egg-info
+#rm -rf dist/
+#rm -rf build/

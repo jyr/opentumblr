1f3f5e08f11a231effed416ed073de5ee289ca8b
Jair Gaxiola <jyr@debian.(none)>
2009-07-27 15:42:04 -0500

    cooking release 0.0.5 at linux

--- uninstall.sh
+++ uninstall.sh
@@ -5,5 +5,7 @@ rm /usr/bin/opentumblr-client.py
 rm -rf /usr/lib/python2.5/site-packages/opentumblr
 rm /usr/share/pixmaps/opentumblr.*
 rm /usr/lib/python2.5/site-packages/opentumblr_client-0.0.4.egg-info
+rm /usr/lib/python2.5/site-packages/tumblr.py
+rm /usr/lib/python2.5/site-packages/tumblr.pyc
 #rm -rf dist/
 #rm -rf build/
